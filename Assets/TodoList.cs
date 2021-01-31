89c67ffefc0bcc303c75aa60a1622c05a99f7f0a
MildaTub <milda.tubyte@gmail.com>
2021-01-31 14:51:29 +0200

    commit

--- Assets/TodoList.cs
+++ Assets/TodoList.cs
@@ -6,47 +6,186 @@ using UnityEngine;
 public class TodoList : MonoBehaviour
 {
     private void OnGUI()
+
     {
-        GUI.Label(new Rect(60, 30, 80, 30), "TO DO LIST:", EditorStyles.boldLabel);
+        GUI.Label(new Rect(60, 20, 100, 20), "TO DO LIST:", EditorStyles.boldLabel);
+
         if (GameManager.CurrentDay == 0)
         {
-            GUI.Label(new Rect(60, 60, 80, 20), "Shave beard", EditorStyles.foldout);
-            GUI.Label(new Rect(60, 80, 80, 20), "Go to the kitchen and eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+            if (GameManager.PlayerShaved)
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Shave beard", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Shave beard", EditorStyles.foldout);
+            }
+
+            if (GameManager.AteFood)
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.foldout);
+            }
+
         }
         else if (GameManager.CurrentDay == 1)
         {
-            GUI.Label(new Rect(0, 0, 80, 20), "Go to the computer in the bedroom and order sports equipment", EditorStyles.foldout);
-            GUI.Label(new Rect(60, 80, 80, 20), "Go to the kitchen and eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+           
+            if (GameManager.ComputerActionDone)
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Order equipment", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Order equipment", EditorStyles.foldout);
+            }
+
+            if (GameManager.AteFood)
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.foldout);
+            }
         }
         else if (GameManager.CurrentDay == 2)
         {
-            GUI.Label(new Rect(0, 0, 80, 20), "Pick up dirty clothing from the bedroom", EditorStyles.foldout);
-            GUI.Label(new Rect(60, 80, 80, 20), "Go to the kitchen and eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+           
+            if (GameManager.LaundryDone)
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Clean dirty clothing", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Clean dirty clothing", EditorStyles.foldout);
+            }
+
+            if (GameManager.AteFood)
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.foldout);
+            }
         }
         else if (GameManager.CurrentDay == 3)
         {
-            GUI.Label(new Rect(0, 0, 80, 20), "Clean up the house and throw out all of the trash", EditorStyles.foldout);
-            GUI.Label(new Rect(60, 80, 80, 20), "Go to the kitchen and eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+           
+            if (GameManager.TrashThrownOut && GameManager.SofaCleaned)
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Clean up the house", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Clean up the house", EditorStyles.foldout);
+            }
+
+            if (GameManager.AteFood)
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.foldout);
+            }
         }
         else if (GameManager.CurrentDay == 4)
         {
-            GUI.Label(new Rect(0, 0, 80, 20), "Exercise with new gym set", EditorStyles.foldout);
-            GUI.Label(new Rect(60, 80, 80, 20), "Go to the kitchen and eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+         
+            if (GameManager.WorkedOut)
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Exercise", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Exercise", EditorStyles.foldout);
+            }
+
+            if (GameManager.AteFood)
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.foldout);
+            }
         }
         else if (GameManager.CurrentDay == 5)
         {
-            GUI.Label(new Rect(0, 0, 80, 20), "Exercise with new gym set twice", EditorStyles.foldout);
-            GUI.Label(new Rect(60, 80, 80, 20), "Go to the kitchen and eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+            
+            if (GameManager.WorkedOut)
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Exercise twice", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Exercise twice", EditorStyles.foldout);
+            }
+
+            if (GameManager.AteFood)
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.foldout);
+            }
         }
         else if (GameManager.CurrentDay == 6)
         {
-            GUI.Label(new Rect(0, 0, 80, 20), "Go to the computer in the bedroom and Order new clothes", EditorStyles.foldout);
-            GUI.Label(new Rect(60, 80, 80, 20), "Go to the kitchen and eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+            
+            if (GameManager.ComputerActionDone)
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Order new clothes", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Order new clothes", EditorStyles.foldout);
+            }
+
+            if (GameManager.AteFood)
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.foldout);
+            }
+
+            if (GameManager.WorkedOut)
+            {
+                GUI.Label(new Rect(60, 80, 100, 20), "Exercise", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 80, 100, 20), "Exercise", EditorStyles.foldout);
+            }
         }
         else if (GameManager.CurrentDay == 7)
         {
-            GUI.Label(new Rect(0, 0, 80, 20), "Try out new clothes", EditorStyles.foldout);
-            GUI.Label(new Rect(60, 80, 80, 20), "Go to the kitchen and eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+           
+            if (GameManager.WearingNewClothes)
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Try out new clothes", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 40, 100, 20), "Try out new clothes", EditorStyles.foldout);
+            }
+
+            if (GameManager.AteFood)
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.centeredGreyMiniLabel);
+            }
+            else
+            {
+                GUI.Label(new Rect(60, 60, 100, 20), "Eat healthy meal", EditorStyles.foldout);
+            }
         }
 
     }
